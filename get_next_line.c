5299fcd35011cdc9d918ee8fb23cf581679b8257
Vincent Zuccarello <vzuccare@student.42lyon.fr>
2023-12-07 20:13:20 +0100

    cc

--- get_next_line.c
+++ get_next_line.c
@@ -6,65 +6,42 @@
 /*   By: vzuccare <vzuccare@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 13:50:02 by vzuccare          #+#    #+#             */
-/*   Updated: 2023/12/01 16:00:06 by vzuccare         ###   ########lyon.fr   */
+/*   Updated: 2023/12/07 13:20:13 by vzuccare         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*check_stash(char *stash)
-{
-	char	*line;
-	int		i;
-
-	i = 0;
-	line = NULL;
-	while (stash[i])
-	{
-		if (stash[i++] == '\n')
-		{
-			ft_strlcat_line(line, stash);
-			return (line);
-		}
-	}
-	return (line);
-}
-
-void	clear_stash(char *stash)
-{
-	char	*str;
-	int		i;
-	int		j;
-
-	i = 0;
-	j = 0;
-	while (stash[i] && stash[i] != '\n')
-		i++;
-	while (stash[j] && stash[j] != '\n')
-		j++;
-	str = malloc(sizeof(char) * (i - j + 1));
-	i = 0;
-	j = 0;
-	while (stash[i] && stash[i] != '\n')
-		stash[j++] = stash[i++];
-	stash[j] = '\0';
-}
-
 char	*get_next_line(int fd)
 {
-	static char	*stash;
-	char		*line;
-	int			i;
+	static char *stash;
+	char *buff;
+	char *line;
 
 	line = NULL;
-	i = 0;
-	while (stash[i] != '\n')
-	{
-		read(fd, stash, BUFFER_SIZE);
-		while (i < BUFFER_SIZE && stash[i] != '\n')
-			i++;
-	}
-	line = ft_strlcat_line(line, stash);
-	clear_stash(stash);
-	return (line);
+	buff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buff)
+		return (NULL);
+	if (!stash)
+		stash = ft_strdup_line(stash, "");
+    while (read(fd, buff, BUFFER_SIZE) > 0)
+    {
+        stash = ft_strjoin(stash, buff);
+		if (!stash)
+		{
+			free(buff);
+			return (NULL);
+		}
+        if (ft_strchr(buff, '\n'))
+            break ;
+    }
+	free(buff);
+    line = ft_strdup_line(line, stash);
+    if (!line)
+    {
+        free(stash);
+        return (NULL);
+    }
+	stash = ft_strchr(stash, '\n');
+	return(line);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,25 +6,82 @@
 /*   By: vzuccare <vzuccare@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 14:17:22 by vzuccare          #+#    #+#             */
-/*   Updated: 2023/12/01 14:21:57 by vzuccare         ###   ########lyon.fr   */
+/*   Updated: 2023/12/07 13:23:02 by vzuccare         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_strlcat_line(char *dst, char *src)
+size_t	ft_strlen(const char *str)
 {
-	int		i;
-	int		j;
+	size_t	i;
 
+	if (!str)
+		return (0);
 	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup_line(char *dst, char *src)
+{
+	size_t		j;
+
 	j = 0;
 	while (src[j] && src[j] != '\n')
-		i++;
-	dst = malloc(sizeof(char) * (i + 1));
-	i = 0;
+		j++;
+	dst = malloc(sizeof(char) * (j + 1));
+	j = 0;
 	while (src[j] && src[j] != '\n')
-		dst[i++] = src[j++];
-	dst[i] = '\0';
+	{
+		dst[j] = src[j];
+		j++;
+	}
+	dst[j] = '\0';
+	if (src)
+		free(src);
 	return (dst);
+}
+char	*ft_strchr( const char *string, int searchedChar )
+{
+	size_t	i;
+
+	i = 0;
+	while (string[i])
+	{
+		if (string[i] == (char)searchedChar)
+			return ((char *)&string[i + 1]);
+		i++;
+	}
+	if (string[i] == (char)searchedChar)
+		return ((char *)&string[i + 1]);
+	if (searchedChar == '\0')
+		return ((char *)&string[i + 1]);
+	return (NULL);
+}
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	i;
+	size_t	size;
+	char	*str;
+
+	i = 0;
+	size = ft_strlen(s1) + ft_strlen(s2);
+	str = malloc((size + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	i = 0;
+	while (s2[i])
+	{
+		str[ft_strlen(s1) + i] = s2[i];
+		i++;
+	}
+	str[ft_strlen(s1) + i] = '\0';
+	return (str);
 }
